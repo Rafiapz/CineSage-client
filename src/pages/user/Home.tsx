7112f7e94581f2cb50157cc5defef6eb904919c9
Muhammed Rafi <rafikandathuvayal@gmail.com>
2024-08-18 17:22:12 +0530

    ready

--- src/pages/user/Home.tsx
+++ src/pages/user/Home.tsx
@@ -1,46 +1,23 @@
-import { FC, useEffect, useState } from "react";
-import ServerDown from "../../components/serverDown/ServerDown";
+import { FC, useEffect } from "react";
 import apiClient from "../../utils/axios";
 import Cards from "../../components/card/Cards";
 
 const Home: FC = () => {
-   const [isLoading, setIsLoading] = useState(true);
-   const [serverIsDown, setServerIsDown] = useState(false);
-
    useEffect(() => {
       fetchMovies();
    });
 
-   useEffect(() => {
-      let timer: any;
-
-      timer = setTimeout(() => {
-         if (isLoading) setServerIsDown(true);
-      }, 12000);
-
-      return () => {
-         clearTimeout(timer);
-      };
-   }, [isLoading]);
-
    const fetchMovies = () => {
-      setIsLoading(true);
-      apiClient.get("/movies/fetch-movies").finally(() => {
-         setIsLoading(false);
-      });
+      apiClient.get("/movies/fetch-movies").finally(() => {});
    };
    return (
       <>
-         {!serverIsDown ? (
-            <div className="mt-8">
-               <div className="w-full h-10 flex justify-center items-center w-lg rounded-md p-4">
-                  <h1 className="text-3xl font-bold text-gray-600 tracking-wide">Now Playing</h1>
-               </div>
-               <Cards />
+         <div className="mt-8">
+            <div className="w-full h-10 flex justify-center items-center w-lg rounded-md p-4">
+               <h1 className="text-3xl font-bold text-gray-600 tracking-wide">Now Playing</h1>
             </div>
-         ) : (
-            <ServerDown />
-         )}
+            <Cards />
+         </div>
       </>
    );
 };
